8b68d0431fbfd03281cf0608ebaf3d5a6d8baca4
Apolki <da12171998@gmail.com>
2020-06-18 14:17:28 +0200

    add animation for move

--- main.js
+++ main.js
@@ -9,10 +9,7 @@ var Dice = /** @class */ (function () {
         this.sides = sides;
     }
     Dice.prototype.roll = function () {
-        var a;
-        a = Math.floor(Math.random() * this.sides) + 1;
-        console.log(a);
-        return a;
+        return Math.floor(Math.random() * this.sides) + 1;
     };
     return Dice;
 }());
@@ -22,9 +19,14 @@ var Player = /** @class */ (function () {
         this.color = color;
         this.position = position;
         this.dice = dice;
+        this.rolls = new Array();
+        this.render_position = position;
     }
     Player.prototype.make_move = function () {
-        return this.position + this.dice.roll();
+        var roll = this.dice.roll();
+        //console.log('rolled: ' + roll + ' position: ' + this.position);
+        this.rolls.push(roll);
+        return this.position += roll;
     };
     return Player;
 }());
@@ -34,6 +36,8 @@ var Board = /** @class */ (function () {
         this.teleports = teleports;
     }
     Board.prototype.apply_teleports = function (position) {
+        // player can only jump once
+        // todo find & apply teleport +-
         for (var _i = 0, _a = this.teleports; _i < _a.length; _i++) {
             var teleport = _a[_i];
             if (position == teleport.source) {
@@ -49,38 +53,95 @@ var Game = /** @class */ (function () {
     function Game(board, players) {
         this.board = board;
         this.players = players;
+        this.turn_number = 0;
+        this.animation_step = 1000;
     }
     Game.prototype.draw = function () {
-        console.log(this.players);
+        // console.log(JSON.stringify(this.players));
+        // console.log(this.players);
+    };
+    Game.prototype.render = function () {
+        // console.log(this);
+        var s = '.'.repeat(this.board.size);
+        for (var _i = 0, _a = this.players; _i < _a.length; _i++) {
+            var player = _a[_i];
+            s = replaceCharAt(s, player.render_position - 1, player.color);
+        }
+        h(s);
+    };
+    Game.prototype.start_animation = function () {
+        for (var _i = 0, _a = this.players; _i < _a.length; _i++) {
+            var player = _a[_i];
+            this.animate(player);
+        }
+    };
+    Game.prototype.animate = function (player) {
+        var _this = this;
+        if (player.render_position < player.position) {
+            player.render_position += 1;
+            this.render();
+            if (player.rolls[0] <= 1) {
+                player.rolls.shift();
+                window.alert('turn end');
+            }
+            else {
+                player.rolls[0] -= 1;
+            }
+            console.log("rolls drop : " + player.name + '  ' + player.rolls);
+            console.log(player.name + ' render position: ' + player.render_position);
+        }
+        setTimeout(function () { _this.animate(player); }, this.animation_step);
     };
     Game.prototype.turn = function () {
-        for (var i = 0; i <= 2; i++) {
-            var new_position = this.players[i].make_move();
-            new_position = this.board.apply_teleports(new_position);
-            this.players[i].position = new_position;
-            console.log(this.players[i].position);
+        for (var _i = 0, _a = this.players; _i < _a.length; _i++) {
+            var player = _a[_i];
+            // get new_position
+            player.make_move();
+            // console.log('#' + this.turn_number + ' new position: ' + new_position);
+            // animate player moving from old_position to new_position
+            // this.animate(player, this.turn_number);
+            // animate player teleporting to new_position
+            // player.position = this.board.apply_teleports(new_position);
+            // this.render();
         }
-        this.draw();
+        this.turn_number++;
+    };
+    Game.prototype.play = function () {
+        var _this = this;
+        setTimeout(function () {
+            _this.turn();
+            _this.turn_number++;
+            if (_this.turn_number < 10) {
+                _this.play();
+            }
+        }, 100);
     };
     return Game;
 }());
 var teleports = [
-    { source: 1, destination: 10 },
-    { source: 2, destination: 20 },
-    { source: 3, destination: 30 },
-    { source: 4, destination: 40 },
-    { source: 5, destination: 50 },
-    { source: 6, destination: 60 },
+    { source: 11, destination: 10 },
+    { source: 21, destination: 20 },
+    { source: 31, destination: 30 },
+    { source: 41, destination: 40 },
+    { source: 51, destination: 50 },
+    { source: 61, destination: 60 },
 ];
 var d6 = new Dice(6);
 var d12 = new Dice(12);
 var players = [
-    new Player("Dan", "Red", 1, d12),
-    new Player("Bot1", "Green", 1, d6),
-    new Player("Bot2", "Blue", 1, d6),
+    new Player("Dan", '@', 1, d12),
+    new Player("Bot1", 'B', 1, d6),
+    new Player("Bot2", 'b', 1, d6),
 ];
 var board = new Board(100, teleports);
 var game = new Game(board, players);
-game.turn();
-game.turn();
-game.turn();
+game.start_animation();
+// setTimeout(() => { game.render(); }, 0);
+function h(s) {
+    document.getElementById('h').innerHTML = s;
+}
+function replaceCharAt(s, i, c) {
+    return s.substring(0, i) + c + s.substring(i + 1);
+    ;
+}
+var on_click = function () { game.turn(); };
--- main.ts
+++ main.ts
@@ -13,17 +13,23 @@ class Dice {
 		) { }
 
 	roll(): number {
-		let a;
-		a = Math.floor(Math.random() * this.sides) + 1;
-		console.log (a);
-		return a;
+		return Math.floor(Math.random() * this.sides) + 1;
 	}
 }
 
 class Player {
-	constructor(public name: string, public color: string, public position: number, public dice: Dice){}
+	render_position: number;
+	rolls = new Array(); 
+	constructor(public name: string, public color: string, public position: number, public dice: Dice) {
+		this.render_position = position;
+	}
 	make_move() {
-		return this.position + this.dice.roll();
+		let roll = this.dice.roll();
+		
+		//console.log('rolled: ' + roll + ' position: ' + this.position);
+		this.rolls.push(roll); 
+		return this.position += roll;
+		
 	}
 }
 
@@ -32,13 +38,14 @@ interface Teleport {
 	destination: number;
 }
 class Board {
-	constructor(public size: number, public teleports: Teleport[]){}
+	constructor(public size: number, public teleports: Teleport[]) {}
 	
-	apply_teleports(position: number): number{
-		
+	apply_teleports(position: number): number {
+		// player can only jump once
+		// todo find & apply teleport +-
 
-		for (let teleport of this.teleports){
-			if (position == teleport.source){
+		for (let teleport of this.teleports) {
+			if (position == teleport.source) {
 				position = teleport.destination;
 				break;
 			}
@@ -47,43 +54,105 @@ class Board {
 	}
 }
 
-
 class Game {
-	constructor(public board: Board, public players: Player[]){}
+	turn_number: number;
+	animation_step: number;
+	constructor(public board: Board, public players: Player[]) {
+		this.turn_number = 0;
+		this.animation_step = 1000;
+	}
 	draw(){
-		console.log(this.players);
-	}	
-	turn() {
-		for (let i=0; i <= 2; i++) {
-			let new_position = this.players[i].make_move();
-			new_position = this.board.apply_teleports(new_position);
-			this.players[i].position = new_position;
-			console.log(this.players[i].position);
+		// console.log(JSON.stringify(this.players));
+		// console.log(this.players);
+		
+	}
+	render() {
+		// console.log(this);
+		var s = '.'.repeat(this.board.size);
+		for (let player of this.players) {
+			s = replaceCharAt(s, player.render_position - 1, player.color);
 		}
-		this.draw();
+		h(s);
+	}
+	start_animation() {
+		for (let player of this.players) {
+			this.animate(player);
+		}
+	}
+	animate(player: Player) {
+		if (player.render_position < player.position) {
+			player.render_position += 1;
+			this.render();
+			if(player.rolls[0] <= 1){
+				player.rolls.shift();
+				window.alert('turn end');				
+			}
+			else{
+				player.rolls[0] -= 1;
+			}
+			console.log("rolls drop : " + player.name + '  ' +  player.rolls);
+			console.log(player.name + ' render position: ' + player.render_position);
+		}
+		setTimeout(() => { this.animate(player); }, this.animation_step);
+	}
+	turn() {
+		for (let player of this.players) {
+			// get new_position
+			player.make_move();
+			// console.log('#' + this.turn_number + ' new position: ' + new_position);
+			// animate player moving from old_position to new_position
+			// this.animate(player, this.turn_number);
+			// animate player teleporting to new_position
+			// player.position = this.board.apply_teleports(new_position);
+			// this.render();
+		}
+		this.turn_number++;
+	}
+	play() {
+		setTimeout(() => {
+			this.turn();
+			this.turn_number++;
+			if (this.turn_number < 10) {
+				this.play();
+			}
+		}, 100);
 	}
 }
 
-
-
 let teleports: Teleport [] = [
-	{source: 1, destination: 10},
-	{source: 2, destination: 20},
-	{source: 3, destination: 30},
-	{source: 4, destination: 40},
-	{source: 5, destination: 50},
-	{source: 6, destination: 60},
+	{source: 11, destination: 10},
+	{source: 21, destination: 20},
+	{source: 31, destination: 30},
+	{source: 41, destination: 40},
+	{source: 51, destination: 50},
+	{source: 61, destination: 60},
 ];
 let d6 = new Dice(6);
 let d12 = new Dice(12);
 let players: Player[] = [
-	new Player("Dan", "Red", 1, d12),
-	new Player("Bot1","Green", 1, d6),
-	new Player("Bot2","Blue", 1,d6),
+	new Player("Dan", '@', 1, d12),
+	new Player("Bot1",'B', 1, d6),
+	new Player("Bot2",'b', 1, d6),
 ];
 let board = new Board (100, teleports);
 let game = new Game(board, players);
-game.turn();
-game.turn();
-game.turn();
+game.start_animation();
+// setTimeout(() => { game.render(); }, 0);
+
+function h(s: string) {
+	document.getElementById('h').innerHTML = s;
+}
+
+
+function replaceCharAt(s: string, i: number, c: string): string {
+	return s.substring(0, i) + c + s.substring(i + 1);;
+}
+
+let on_click = () => { game.turn(); };
+ 
+
+
+
+
+
 
